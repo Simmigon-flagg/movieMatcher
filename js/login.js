22073cf899fbb04666e6933e4d444da919be1f17
Simmigon <simmigon.flagg@gmail.com>
2018-08-21 15:32:49 -0400

    Login shell

--- js/login.js
+++ js/login.js
@@ -1,1 +1,12 @@
 console.log("login.js");
+let signIn = document.getElementById('signin');
+//Listening on thesignIn button click.
+signIn.addEventListener('click', (e) => {
+    let email = document.getElementById('inputEmail').value;
+    let password = document.getElementById('inputPassword').value;
+    e.preventDefault();
+    if (email === "admin@admin.com" && password === "admin") {
+        window.location.href = 'sites/home.html';
+    }
+
+});
