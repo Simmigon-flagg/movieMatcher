1dea04585b2e664faab9f522f8a11e863053c7d9
Simmigon <simmigon.flagg@gmail.com>
2018-08-26 01:19:47 -0400

    If applied this users can search different platform for the movie they search

--- js/signup.js
+++ js/signup.js
@@ -2,12 +2,12 @@ console.log("Signup");
 // Initialize Firebase
 
 var config = {
-    apiKey: "AIzaSyD_5x9ul3ohecp5fJedYIQfaEpPqsyZ8WI",
-    authDomain: "auth-test-f7d87.firebaseapp.com",
-    databaseURL: "https://auth-test-f7d87.firebaseio.com",
-    projectId: "auth-test-f7d87",
-    storageBucket: "auth-test-f7d87.appspot.com",
-    messagingSenderId: "1059612843083"
+    apiKey: "AIzaSyA4DnsmunTp-DWFIjWJ0NvZfSxIojCXUIU",
+    authDomain: "moviematcher-2909e.firebaseapp.com",
+    databaseURL: "https://moviematcher-2909e.firebaseio.com",
+    projectId: "moviematcher-2909e",
+    storageBucket: "moviematcher-2909e.appspot.com",
+    messagingSenderId: "411775304326"
 };
 firebase.initializeApp(config);
 
@@ -16,12 +16,17 @@ let signup = document.getElementById('sign-up-validate');
 
 //Listening on thesignIn button click.
 signup.addEventListener('click', (ev) => {
+   
     let email = document.getElementById('signupemail').value;
+     alert(email);
+    console.log(signup);
     let password = document.getElementById('passsword1').value;
- 
+    console.log(signup);
     ev.preventDefault();
+
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
         // Handle Errors here.
+        // console.log(email, password);
         var errorCode = error.code;
         console.log(errorCode);
 
@@ -31,6 +36,10 @@ signup.addEventListener('click', (ev) => {
         // ...
     });
     if (true) {
+
         window.location.href = 'home.html';
+
     }
 });
+
+console.log("End");
