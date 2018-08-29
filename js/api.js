eceaeb566f807f8326f850b8556d536e60bee074
sungylee <sungylee@yahoo.com>
2018-08-29 10:52:19 -0400

    added comment, formatted, and added missing image class stream-con

--- js/api.js
+++ js/api.js
@@ -1,38 +1,39 @@
-
-
-// constructing a queryURL variable we will use instead of the literal string inside of the ajax method
+// Global Array to capture from omdbapi query
 var movieTitleRelease = [];
-var movieNameRelease = [];
 
 
-
+//Function  AJAX query to omdbapi and capture movie title, and year of release
+//Input movie - search term
 function searchDate(movie) {
-  
- 
   var queryURL = "https://www.omdbapi.com/?s=" + movie + "&y=&plot=short&apikey=trilogy";
-    
-      // Creating an AJAX call for the specific movie button being clicked
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) { 
-     // console.log(response);
-      
-      var movieResults = response.Search;
-       for (i=0; i < movieResults.length; i++) {
-         var movieList = movieResults[i].Title;
-         var movieReleaseDate = movieResults[i].Year;
-      //   console.log(movieList);
-      //   console.log(movieReleaseDate);
-         movieTitleRelease.push(movieList);
-         movieTitleRelease.push(movieReleaseDate);
-        }
-      });
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) { 
+
+    //JSON "Search"
+    //            "Title"
+    //            "Year"
+
+    var movieResults = response.Search;
+      //Loop until end of Search length and capture Movie title, year
+      for (i=0; i < movieResults.length; i++) {
+        var movieList = movieResults[i].Title;
+        var movieReleaseDate = movieResults[i].Year;
+        //Add to movieTitleRelease array
+        movieTitleRelease.push(movieList);
+        movieTitleRelease.push(movieReleaseDate);
+      }
+    });
         
-    }
-    
+}
+
+//Funtion AJAX to utelly and capture movie title, poster, where to watch
+//Input movie - search term
 function searchMovie(movie) {
   var queryURL = "https://utelly-tv-shows-and-movies-availability-v1.p.mashape.com/lookup?country=us&term=" + movie + "";
+  // Creating an AJAX call for the specific movie button being clicked
   $.ajax({
     url: queryURL,
     method: "GET",
@@ -40,81 +41,83 @@ function searchMovie(movie) {
     headers: { "X-Mashape-Key": "ETvaxKZbFhmshL3jAAZP4Ylhx0iYp1v9LNHjsnY3CSBvSlVwgt" }
 
   }).then(function (response) {
-    $("#movies").empty();
-    var count = 0;
+   
     var result = response.results;
     var releaseYear;
     var j = 0;
 
-    console.log(result);
+    //clear html id movies
+    $("#movies").empty();
+  
+    //old school jQuery each() Method to loop results get Title,picture
     $.each(result, function (index, value) {
-      // console.log(result[index].name);
-      // console.log(result[index]);
       
-      
-      // console.log(index + ": " + value);
-      var topicDIV = $("<p>");
-      // Creating a paragraph tag with the result item's rating
-      var title = $("<p>").text("Title: " + result[index].name.toUpperCase());
+    //JSON "term"
+    //            "results"
+    //                     "name"
+    //                     "locations"
+    //                                "name"
+    //                                "url"
+    //                                "icon"
+    
+   var topicDIV = $("<p>");
+   var title = $("<p>").text("Title: " + result[index].name.toUpperCase());
+   var topicImage = $("<img>");
 
-      var topicImage = $("<img>");
-      topicImage.attr("src", result[index].picture);
-      topicDIV.append(title);
-      topicDIV.append(topicImage);
+   topicImage.attr("src", result[index].picture);
+   topicDIV.append(title);
+   topicDIV.append(topicImage);
 
+   var streaming = result[index].locations;
+   var headerText = $("<p>").text("Streaming Platform:");
+    
+    //Perform comparsion of results from omdbapi and utelly of movie title and capture year
+    for (var j=0; j < movieTitleRelease.length ; j++) {
+      if (movieTitleRelease[j].toLowerCase() === result[index].name.toLowerCase()) {
+        releaseYear = movieTitleRelease[j+1];
+        var Year = $("<p>").text("Year: " + releaseYear);
+        topicDIV.append(Year);
+        //breaks the loop and continues executing the code after the loop 
+        break;
+      }
+      if (movieTitleRelease.length === j+1) {
+        var Year = $("<p>").text("Year: NA");
+        topicDIV.append(Year);
+      } 
+    };
+
+      //JSON "term"
+      //            "results"
+      //                     "name"
+      //                     "locations"
+      //                                "name"
+      //                                "url"
+      //                                "icon"
       var streaming = result[index].locations;
       var headerText = $("<p>").text("Streaming Platform:");
-    
-     
-//for comparison
-      for (var j=0; j < movieTitleRelease.length ; j++) {
-       // console.log("moviename from OD" +movieTitleRelease[j]);
-       // console.log("moviename" + result[index].name);
-   
-        if (movieTitleRelease[j].toLowerCase() === result[index].name.toLowerCase()) {
-          releaseYear = movieTitleRelease[j+1];
-          var Year = $("<p>").text("Year: " + releaseYear);
-          topicDIV.append(Year);
-          break;
-          console.log(releaseYear);
-        }
-        
-        if (movieTitleRelease.length === j+1) {
-        
-          var Year = $("<p>").text("Year: NA");
-          topicDIV.append(Year);
-          
+      //var url = result[index].locations.url;
 
-        } 
-
-
-      };
-      var headerText = $("<p>").text("Streaming Platform:");
       topicDIV.append(headerText);
-      // 
 
-      var url = result[index].locations.url;
-
-    
+      //old school jQuery each() Method to loop results  get where to watch name, url, icon
       $.each(streaming, function (index, value) {
-        var topicImage = $("<img>");
-        topicImage.attr("src", streaming[index].icon);
+
+        var streamingIcon = streaming[index].icon;
+        var streamingHref = streaming[index].url;
+        var topicImage = $('<img class="stream-icon">');
+
+        topicImage.attr("src", streamingIcon);
 
         var movieStream = $("<a>");
-        movieStream.attr("href", streaming[index].url);
-
+        movieStream.attr("href", streamingHref);
         movieStream.append(topicImage);
-
         topicDIV.append(movieStream);
         // topicDIV.append(movieStream);
       });
+  
+   $("#movies").prepend(topicDIV);
 
-         
-
-      $("#movies").prepend(topicDIV);
-
-    });
- 
+   });
 
   });
 
@@ -130,10 +133,10 @@ $("#searchapidata").on("click", function (event) {
   // Running the  function (passing in the movie as argument)
   searchDate(inputMovie);
 
-     //  Set the button alert's timeout to run three seconds after the function's called.
-     delayButtonAlert = setTimeout(function() {
-      
-    }, 3000);
+  //Set the button alert's timeout to run three seconds after the function's called.
+  delayButtonAlert = setTimeout(function() {
+     
+   }, 3000);
 
   searchMovie(inputMovie);
  // $("#searchterm").val("");
